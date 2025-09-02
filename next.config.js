7c3abf2c653270f8094c41cff52a77a1ce11ff88
Youness JELLOULI <younessjellouli12@gmail.com>
2025-09-02 01:47:16 +0100

    .

--- next.config.js
+++ next.config.js
@@ -6,7 +6,10 @@ const nextConfig = {
         source: "/api/:path*",
         headers: [
           { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET, POST, PUT, DELETE, OPTIONS, PATCH" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS, PATCH",
+          },
           { key: "Access-Control-Allow-Headers", value: "*" },
           { key: "Access-Control-Allow-Credentials", value: "false" },
           { key: "Access-Control-Max-Age", value: "86400" },
